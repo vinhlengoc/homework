818b3904b470b07732105c5c67e73f9365a35e16
Le Ngoc Vinh <lvinh@hyperion.co.jp>
2020-05-23 15:03:11 +0700

    Initial Commit

---
+++ SigmaHomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SigmaHomeWork
+//
+//  Created by Le Ngoc Vinh on 5/23/20.
+//  Copyright © 2020 vinhln. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
